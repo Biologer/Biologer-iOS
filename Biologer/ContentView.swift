873c5fd22bced757d06d629dd2e99d65006bb501
Nikola <nikolapopovic993@yahoo.com>
2021-04-17 19:34:48 +0200

    Initial Commit

---
+++ Biologer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Biologer
+//
+//  Created by Nikola Popovic on 17.4.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
