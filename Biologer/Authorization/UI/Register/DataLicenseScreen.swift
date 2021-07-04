aa67000e77f424f6da98278f5cea6286612934c9
Nikola <nikolapopovic993@yahoo.com>
2021-07-04 17:57:38 +0200

    spread authorization router

---
+++ Biologer/Authorization/UI/Register/DataLicenseScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DataLicenseScreen.swift
+//  Biologer
+//
+//  Created by Nikola Popovic on 4.7.21..
+//
+
+import SwiftUI
+
+struct DataLicenseScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataLicenseScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DataLicenseScreen()
+    }
+}
