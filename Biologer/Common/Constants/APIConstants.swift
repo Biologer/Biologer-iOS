5de68cca36fa5b750bbd714fbb7b9fc82091be46
Jelena Krmar <jelenakrmar@gmail.com>
2025-03-17 21:04:11 +0100

    initial commit - api constants added to git, .DS_Store removed

---
+++ Biologer/Common/Constants/APIConstants.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public final class APIConstants {
+    public static let applicationJson = "application/json"
+    public static let grantTypeRefreshToken = "refresh_token"
+    public static let grantTypePassword = "password"
+    public static let scope = "*"
+    
+    public static let loginUserPath = "/oauth/token"
+    public static let taxonPath = "/api/taxa"
+    public static let registerUserPath = "/api/register"
+    public static let myProfilePath = "/api/my/profile"
+    public static let observationsPath = "/api/field-observations"
+    
+    public static let uploadFindingImagePath = "/api/uploads/photos"
+    public static let uploadFindingPath = "/api/field-observations"
+    
+    public static let getAltitudePath = "/api/elevation"
+}
