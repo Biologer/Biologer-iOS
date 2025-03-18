a9562eaf1e41bd25dd97da7db0c9fe2eae4517dc
Jelena Krmar <jelenakrmar@gmail.com>
2025-03-18 23:54:15 +0100

    maps key removed from git

--- Biologer/AppDelegate.swift
+++ Biologer/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyDt4_dL2dW-IU4tarf-NySm2Do-AYw_urg")
-        GMSPlacesClient.provideAPIKey("AIzaSyDt4_dL2dW-IU4tarf-NySm2Do-AYw_urg")
+        GMSServices.provideAPIKey(GoogleMapsKey)
+        GMSPlacesClient.provideAPIKey(GoogleMapsKey)
         return true
     }
 
