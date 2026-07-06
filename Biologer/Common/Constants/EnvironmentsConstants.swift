b445cafa4917900cead06a37970bb8b0cf7e846e
Nikola Popovic <nikolapopovicdeveloper@gmail.com>
2026-07-06 12:58:16 +0200

    Migrate dependencies to Swift Package Manager

---
+++ Biologer/Common/Constants/EnvironmentsConstants.swift
@@ -0,0 +1,34 @@
+//
+//  EnvironmentConstants.swift
+//  Biologer
+//
+//  Created by Nikola Popovic on 14.7.21..
+//
+
+import Foundation
+
+let GoogleMapsKey = ""
+
+let serbiaHost = ""
+let croatiaHost = ""
+let bosnianAndHerzegovinHost = ""
+let montenegroHost = ""
+let devHost = ""
+
+let serbiaPath = ""
+let croatiaPath = ""
+let bosnianAndHerzegovinaPath = ""
+let montenegroPath = ""
+let devPath = ""
+
+let cliendIdDev = ""
+let cliendIdSer = ""
+let cliendIdCro = ""
+let cliendIdBih = ""
+let cliendIdMe = ""
+
+let serbiaClientSecret = ""
+let croatiaClientSecret = " "
+let bosnianAndHercegovinaClientSecret = ""
+let montenegroClientSecret = ""
+let devClientSecret = ""
